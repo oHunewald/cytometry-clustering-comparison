0d0c1fa3eed955812b83ad4c3b3f9284b90c0972
ohunewald <oliver.hunewald@lih.lu>
2020-01-02 09:24:53 +0100

    added dict for filenames and marker ranges

--- run_methods/run_GigaSOM.jl
+++ run_methods/run_GigaSOM.jl
@@ -1,5 +1,5 @@
 #=
-Julia script to run GigaSOM
+Julia script to run GigaSOM. This script is based on L.Weber: run_FlowSOM
 
 Oliver Hunewald, January 2020
 =#
@@ -11,5 +11,15 @@ checkDir()
 cwd = pwd()
 
 datapath = "../../"
+
+cd(datapath)
+
+data = Dict("Levine_32dim.fcs" => [5:36;],
+            "Levine_13dim.fcs" => [1:13;],
+            "Samusik_01.fcs" => [9:47;],
+            "Samusik_all.fcs" => [9:47;],
+            "Nilsson_rare" => [5:7; 9:18],
+            "Mosmann_rare.fcs" => [7:9; 11:21])
+
 
 println("Hello World!")
