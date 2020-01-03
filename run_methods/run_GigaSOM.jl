9d7b0f775186eaa900573a52575a9fd95326c81a
ohunewald <oliver.hunewald@lih.lu>
2020-01-03 14:06:33 +0100

    added single file processing

--- run_methods/run_GigaSOM.jl
+++ run_methods/run_GigaSOM.jl
@@ -10,7 +10,7 @@ checkDir()
 #create genData and data folder and change dir to dataPath
 cwd = pwd()
 
-datapath = "../../"
+datapath = "/Users/ohunewald/work/GigaSOM_data/validation_results/Weber_cytof_comparison"
 
 cd(datapath)
 
@@ -26,7 +26,51 @@ data = Dict("Levine_32dim.fcs" => [5:36;],
 # end
 
 
-# fcsRaw = readFlowset("Nilsson_rare.fcs")
-# cleanNames!(fcsRaw)
+fcsRaw = readFlowFrame("Levine_32dim.fcs")
+cleanNames!(fcsRaw)
+
+daf = fcsRaw["Levine_32dim.fcs"]
+daf_sub = daf[:, 5:36]
+p = addprocs(2)
+
+@everywhere using GigaSOM
+
+som2 = initGigaSOM(daf_sub, 10, 10)
+@time som2 = trainGigaSOM(som2, daf_sub, epochs = 10)
+winners = mapToGigaSOM(som2, daf_sub)
+CSV.write("clustering_Levine_32dim_GigaSOM.csv", winners)
+
+
+rmprocs(workers())
+
+# extract the codes for consensus clustering
+codes = som2.codes
+
+try
+	using RCall
+catch
+	using Pkg
+	Pkg.add("RCall")
+end
+using RCall
+using Statistics
+using LinearAlgebra
+Pkg.add("StatsBase")
+using StatsBase
+@rlibrary consens2
+
+plot_outdir = "consensus_plots"
+nmc = 40
+codesT = Matrix(codes')
+
+mc = ConsensusClusterPlus_2(codesT, maxK = nmc, reps = 100,
+							pItem = 0.9, pFeature = 1,
+							clusterAlg = "hc", innerLinkage = "average", finalLinkage = "average",
+							distance = "euclidean", seed = 1234)
+
+cell_clustering = mc[winners.index]
+# cell_clustering = convert(DataFrame, cell_clustering')
+cc = DataFrame(id = cell_clustering)
+CSV.write("clustering_Levine_32dim_GigaSOM_40_metaCluster.csv", cc)
 
 println("Hello World!")
